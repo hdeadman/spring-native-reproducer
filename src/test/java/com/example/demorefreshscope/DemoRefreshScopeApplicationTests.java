7748bea5ea9a188ad35e8a55affd42ffd9f5a849
hdeadman <hal.deadman@gmail.com>
2022-01-23 21:43:27 -0500

    add server.port property in test so maven build passes

--- src/test/java/com/example/demorefreshscope/DemoRefreshScopeApplicationTests.java
+++ src/test/java/com/example/demorefreshscope/DemoRefreshScopeApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.demorefreshscope;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"server.port=8080"})
 class DemoRefreshScopeApplicationTests {
 
 	@Test
