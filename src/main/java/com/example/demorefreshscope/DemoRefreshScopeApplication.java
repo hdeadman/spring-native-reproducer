6bc2ebd27c66ca941dea5dc02a96a4c0f6e4471e
Hal Deadman <hal.deadman@gmail.com>
2022-03-03 20:46:39 -0500

    bootstrap issue

--- src/main/java/com/example/demorefreshscope/DemoRefreshScopeApplication.java
+++ src/main/java/com/example/demorefreshscope/DemoRefreshScopeApplication.java
@@ -1,12 +1,35 @@
 package com.example.demorefreshscope;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+import java.time.Instant;
+
+@SpringBootApplication(proxyBeanMethods = false, exclude = {
+		DataSourceAutoConfiguration.class,
+		MongoAutoConfiguration.class,
+		MongoDataAutoConfiguration.class
+})
+
 public class DemoRefreshScopeApplication {
 
 	public static void main(String[] args) {
+		new SpringApplicationBuilder(DemoRefreshScopeApplication.class)
+				.web(WebApplicationType.SERVLET)
+				.logStartupInfo(true)
+				.run(args);
 		SpringApplication.run(DemoRefreshScopeApplication.class, args);
 	}
 
--- test-maven.sh
+++ test-maven.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo Buidling with maven
 ./mvnw clean package
 
