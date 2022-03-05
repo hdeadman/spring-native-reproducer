18adddc03f2b02288b593bd7b29b4da8fb3ae7b5
hdeadman <hal.deadman@gmail.com>
2022-03-04 19:13:30 -0500

    make more minimal

---
+++ src/main/java/com/example/demorefreshscope/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demorefreshscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(DemoApplication.class)
+				.web(WebApplicationType.SERVLET)
+				.logStartupInfo(true)
+				.run(args);
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
--- src/test/java/com/example/demorefreshscope/DemoApplicationTests.java
+++ src/test/java/com/example/demorefreshscope/DemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {"server.port=8080"})
-class DemoRefreshScopeApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
